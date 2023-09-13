5bba9403a5d1fd4fd30b713fbc05eb865e97bada
Niloy Nath <skanniloynath@gmail.com>
2023-09-14 01:11:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,30 +2,26 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 import numpy as np
+from fpdf import FPDF
+from datetime import datetime
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 import base64
 import requests
-
-# Page config
-st.set_page_config(
-    page_title='EarthFinesse - Military Terrain Classifier',
-    page_icon='🌍',
-    layout='wide',
-    initial_sidebar_state='auto'
-)
+import tempfile
+import os
 
 # Load model
 model = load_model('terrain__2023_09_13__11_52_06___Accuracy_0.9787.h5')
 
 # Class labels
-label_map = {0: 'Grassy', 1: 'Marshy', 2: 'Rocky', 3: 'Sandy'}
+label_map = {0: 'Sandy', 1: 'Marshy', 2: 'Rocky', 3: 'Grassy'}
 
 # Define military camo theme colors
-bg_color = "#0D3B66"  # Navy blue background
+bg_color = "#383838"  # Dark background
 text_color = "#FFFFFF"  # White text
-accent_color = "#F4A261"  # Amber accent color
+accent_color = "#4CAF50"  # Military green accent color
 
 # Apply the theme
 st.markdown(
@@ -41,141 +37,174 @@ st.markdown(
     .widget-label {{
         color: {text_color};
     }}
-    .stButton {{
+    .stButton>button {{
         color: {text_color};
         background-color: {accent_color};
-        border-color: {accent_color};
-    }}
-    .stButton:hover {{
-        background-color: {text_color};
-        color: {accent_color};
     }}
     </style>
     """,
     unsafe_allow_html=True
 )
 
-# Tabs for File Upload and Map
-tabs = st.sidebar.radio("Select Operation", ("File Upload", "Map Coordinates"))
+class PDF(FPDF):
+    def chapter_title(self, title):
+        self.set_font('Arial', 'B', 12)
+        self.cell(0, 10, title, 0, 1, 'L')
+        self.ln(4)
 
-if tabs == "File Upload":
-    # File Upload Tab
-    st.title('🌍 EarthFinesse - Military Terrain Classifier 🛡️')
-    st.header('File Upload and Classification')
+    def chapter_body(self, body, image_path):
+        # Add an image
+        self.image(image_path, x=10, w=80)
 
-    # Sidebar options
-    st.sidebar.header('Mission Settings')
-    threshold = st.sidebar.slider('Confidence Threshold', 0.0, 1.0, 0.5, 0.01, help="Adjust the prediction threshold for classifying images.")
-    show_probabilities = st.sidebar.checkbox('Show Probabilities', False, help="Display prediction probabilities along with terrain labels.")
-    bulk_classification = st.sidebar.checkbox('Bulk Classification', False, help="Enable batch classification for multiple images.")
-    
-    if bulk_classification:
-        st.sidebar.write("Upload multiple images to classify in bulk.")
+        self.set_font('Arial', '', 12)
+        self.multi_cell(0, 10, body)
+        self.ln()
 
-    # File uploader to accept multiple images
-    uploaded_files = st.file_uploader('Upload Reconnaissance Images', type=['png', 'jpg'], accept_multiple_files=True, help="Select one or more reconnaissance images.")
+def classify_image(img):
+    img = img.resize((224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    prediction = model.predict(x)
+    label_index = np.argmax(prediction)
+    prediction_prob = prediction[0, label_index]
+    return label_map[label_index], prediction_prob
 
-    if uploaded_files:
-        # Initialize an empty list to store bulk classification results
-        bulk_results = []
-
-        # Process each uploaded image
-        for file in uploaded_files:
-            # Load and preprocess image
-            img = Image.open(file)
+def classify_image_from_url(image_url):
+    try:
+        response = requests.get(image_url, stream=True)
+        if response.status_code == 200:
+            img = Image.open(response.raw)
             img = img.resize((224, 224))
             x = image.img_to_array(img)
             x = np.expand_dims(x, axis=0)
             x = preprocess_input(x)
-
-            # Make prediction
             prediction = model.predict(x)
             label_index = np.argmax(prediction)
             prediction_prob = prediction[0, label_index]
+            return label_map[label_index], prediction_prob
+        else:
+            return "Unknown", 0.0
+    except Exception as e:
+        return "Error", 0.0
 
-            # Determine if the prediction is uncertain
-            uncertain = prediction_prob < 0.25
+def generate_pdf_report(df):
+    pdf = PDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    pdf.cell(0, 10, "EarthFinesse Military Terrain Classification Report", ln=True, align="C")
+    pdf.ln(10)
+    pdf.rect(5.0, 5.0, 200.0, 280.0)
+    pdf.set_font("Arial", size=10)
+    pdf.cell(0, 10, "Bulk Classification Results", ln=True, align="L")
+    pdf.ln(10)
 
-            # Display image and result
-            col1, col2 = st.columns([2, 1])
-            with col1:
-                st.image(img, use_column_width=True, caption="Reconnaissance Image")
-            with col2:
-                if uncertain:
-                    st.error('Uncertain Terrain')
-                elif prediction_prob >= threshold:
-                    st.success(f'Terrain: {label_map[label_index]}')
-                    if show_probabilities:
-                        st.write(f'Confidence: {prediction_prob:.2%}')
-                else:
-                    st.error('Uncertain Terrain')
+    current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    pdf_file_name = f"EarthFinesse_Classification_Report_{current_datetime}.pdf"
 
-            # Append the result to the bulk results list
-            bulk_results.append({
-                'FileName': file.name,
-                'Terrain': label_map[label_index] if not uncertain else 'Uncertain Terrain',
-                'Confidence': prediction_prob
-            })
+    for i, (_, row) in enumerate(df.iterrows()):
+        terrain, confidence = row["Terrain"], row["Confidence"]
+        image_path = row["Image"]
+        body = f"Terrain: {terrain}\nConfidence: {confidence:.2%}"
 
-        # Offer bulk classification
+        pdf.chapter_body(body, image_path)
+
+    pdf_file_path = os.path.join("pdf_reports", pdf_file_name)
+    os.makedirs("pdf_reports", exist_ok=True)
+
+    pdf.output(pdf_file_path)
+
+    return pdf_file_path
+
+
+def main():
+    st.sidebar.title('Select Operation')
+    operation = st.sidebar.radio("Choose an operation:", ("File Upload", "Map Coordinates"))
+
+    if operation == "File Upload":
+        st.title('🌍 EarthFinesse - Military Terrain Classifier 🛡️')
+        st.header('File Upload and Classification')
+        
+        st.sidebar.header('Mission Settings')
+        threshold = st.sidebar.slider('Confidence Threshold', 0.0, 1.0, 0.5, 0.01)
+        show_probabilities = st.sidebar.checkbox('Show Probabilities', False)
+        bulk_classification = st.sidebar.checkbox('Bulk Classification', False)
+
         if bulk_classification:
-            st.sidebar.header('Bulk Classification Report')
+            st.sidebar.write("Upload multiple images to classify in bulk.")
+        else:
+            st.sidebar.write("Upload a single image for classification.")
 
-            # Create a DataFrame from bulk results
-            df = pd.DataFrame(bulk_results)
-            
-            # Show the images in the report
-            def get_image_download_link(index):
-                img = Image.open(uploaded_files[index])
-                img.thumbnail((100, 100))
-                img_encoded = base64.b64encode(img.tobytes()).decode()
-                href = f'<a href="data:image/png;base64,{img_encoded}" download="image.png">Download Image</a>'
-                return href
+        uploaded_files = st.file_uploader('Upload Reconnaissance Images', type=['png', 'jpg'], accept_multiple_files=True, help="Select one or more reconnaissance images.")
 
-            df['Image'] = [get_image_download_link(i) for i in range(len(uploaded_files))]
-            st.dataframe(df)
+        if uploaded_files:
+            bulk_results = []
+            st.markdown("---")
 
-else:
-    # Map Coordinates Tab
-    st.title('🌍 EarthFinesse - Military Terrain Classifier 🛡️')
-    st.header('Map Coordinates and Terrain Classification')
+            for i, file in enumerate(uploaded_files):
+                st.subheader(f"Image {i+1}")
+                st.image(file, caption="Reconnaissance Image", use_column_width=True)
 
-    # Sidebar options for map coordinates
-    st.sidebar.header('Mission Settings - Map Coordinates')
-    place_name = st.sidebar.text_input("Enter a location (e.g., city or landmark):", help="Enter the location you want to explore.")
-    search_button = st.sidebar.button("Search", help="Click to search for the location on the map and classify terrain.")
+                terrain, confidence = classify_image(Image.open(file))
 
-    if search_button and place_name:
-        try:
-            # Use OpenStreetMap Nominatim API to get the coordinates of the place
-            osm_api_url = f"https://nominatim.openstreetmap.org/search?format=json&q={place_name}"
-            response = requests.get(osm_api_url)
-            data = response.json()
+                # Save the image to a temporary file in PNG format
+                with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_image:
+                    img = Image.open(file)
+                    img.save(temp_image, format="PNG")
+                    temp_image_path = temp_image.name
 
-            if len(data) > 0:
-                # Extract latitude and longitude from the response
-                latitude = float(data[0]['lat'])
-                longitude = float(data[0]['lon'])
+                bulk_results.append({
+                    'Image': temp_image_path,
+                    'Terrain': terrain,
+                    'Confidence': confidence
+                })
 
-                st.subheader(f"Location Details:")
-                st.write(f"Latitude: {latitude}")
-                st.write(f"Longitude: {longitude}")
+            if bulk_results:
+                st.sidebar.markdown("---")
+                st.sidebar.header('Generate PDF Report')
+                if st.sidebar.button("Download PDF Report"):
+                    pdf_file_path = generate_pdf_report(pd.DataFrame(bulk_results))
+                    st.sidebar.success(f"PDF report generated! [Download PDF]({pdf_file_path})")
 
-                # Display an embedded OpenStreetMap iframe with location
-                osm_map_url = f"https://www.openstreetmap.org/?mlat={latitude}&mlon={longitude}#map=15/{latitude}/{longitude}"
-                st.subheader(f"🗺️ Location Map:")
-                st.markdown(f'<iframe width="600" height="450" frameborder="0" style="border:0" src="{osm_map_url}" allowfullscreen></iframe>', unsafe_allow_html=True)
+    else:
+        st.title('🌍 EarthFinesse - Military Terrain Classifier 🛡️')
+        st.header('Map Coordinates and Terrain Classification')
+        
+        st.sidebar.header('Mission Settings - Map Coordinates')
+        place_name = st.sidebar.text_input("Enter a location (e.g., city or landmark):")
+        zoom_level = st.sidebar.slider("Zoom Level", 1, 18, 10)
+        search_button = st.sidebar.button("Search")
 
-                # Prepare the coordinates for terrain classification
-                # You can implement terrain classification logic here
+        if search_button and place_name:
+            try:
+                # Use OpenStreetMap Nominatim API to get the coordinates of the place
+                osm_api_url = f"https://nominatim.openstreetmap.org/search?format=json&q={place_name}"
+                response = requests.get(osm_api_url)
+                data = response.json()
 
-            else:
-                st.error(f"No coordinates found for the provided location: {place_name}")
+                if len(data) > 0:
+                    latitude = float(data[0]['lat'])
+                    longitude = float(data[0]['lon'])
 
-        except Exception as e:
-            st.error(f"An error occurred: {str(e)}")
+                    st.subheader(f"Location Details:")
+                    st.write(f"Latitude: {latitude}")
+                    st.write(f"Longitude: {longitude}")
 
-# Footer
-st.sidebar.header('About EarthFinesse 🌍🛡️')
-st.sidebar.write("EarthFinesse is a military terrain classification tool designed for reconnaissance purposes. It identifies terrain types, such as Grassy, Marshy, Rocky, and Sandy, in images.")
-st.sidebar.write("Built for the defense community by \nThe Syntax Slingers")
+                    st.subheader(f"🛰️ Satellite Imagery of {place_name}:")
+                    google_maps_url = f"https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d28961.509524683654!2d{longitude}!3d{latitude}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sus!4v1630323443061!5m2!1sen!2sus"
+                    st.markdown(f'<iframe width="600" height="450" frameborder="0" style="border:0" src="{google_maps_url}" allowfullscreen></iframe>', unsafe_allow_html=True)
+
+                    terrain, confidence = classify_image_from_url(google_maps_url)
+                    st.subheader('🌲 Predicted Terrain Type:')
+                    st.write(f'🌿 {terrain}')
+                    st.subheader('🎯 Prediction Accuracy:')
+                    st.write(f'📊 {confidence * 100:.2f}%')
+
+                else:
+                    st.error(f"No coordinates found for the provided location: {place_name}")
+
+            except Exception as e:
+                st.error(f"An error occurred: {str(e)}")
+
+if __name__ == "__main__":
+    main()
